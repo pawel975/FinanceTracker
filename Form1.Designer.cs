840c7b11281d61c04ae029dc5283d12820ecc666
Paweł Kurek <pawel.kurek975@gmail.com>
2024-04-06 14:49:18 +0200

    Setup of .gitignore files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.openFileDialogExcelSheet = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // openFileDialogExcelSheet
+            // 
+            this.openFileDialogExcelSheet.FileName = "openFileDialog1";
+            this.openFileDialogExcelSheet.Filter = "Excel Files (*.xlsx;*.xlsm)|*.xlsx;*.xlsm|All Files (*.*)|*.*";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open Excel File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 127);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(604, 484);
+            this.dataGridView1.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(915, 462);
+            this.ClientSize = new System.Drawing.Size(1104, 634);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialogExcelSheet;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using ClosedXML.Excel;
+using System;
 using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FinanceTracker
@@ -15,6 +11,47 @@ namespace FinanceTracker
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialogExcelSheet.ShowDialog() == DialogResult.OK)
+                ReadSpreadSheetToGrid();
+             
+        }
+
+        private void ReadSpreadSheetToGrid()
+        {
+            try
+            {
+                var workbook = new XLWorkbook(openFileDialogExcelSheet.FileName);
+                var worksheet = workbook.Worksheets.First();
+                var dataTable = new DataTable();
+
+                // Add columns based on the first row (header row)
+                foreach (var headerCell in worksheet.Row(1).Cells())
+                {
+                    dataTable.Columns.Add(headerCell.Value.ToString(), typeof(string));
+                }
+
+                // Add data rows from the worksheet (skip the header row)
+                for (int row = 2; row <= worksheet.Rows().Count(); row++)
+                {
+                    var dataRow = dataTable.NewRow();
+                    for (int col = 1; col <= worksheet.Columns().Count(); col++)
+                    {
+                        dataRow[col - 1] = worksheet.Cell(row, col).Value.ToString();
+                    }
+                    dataTable.Rows.Add(dataRow);
+                }
+
+                dataGridView1.DataSource = dataTable;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Extension 'sql' is not supported. " +
+                    "Supported extensions are '.xlsx', '.xlsm', '.xltx' and '.xltm'.");
+            }
         }
     }
 }
