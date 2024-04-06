560074945406c35e747ea35d70574af50980440a
Paweł Kurek <pawel.kurek975@gmail.com>
2024-04-06 20:28:23 +0200

    Cleanup of methods

--- EventHandlersFunctions/GeneratePDFReport.cs
+++ EventHandlersFunctions/GeneratePDFReport.cs
@@ -1,11 +1,33 @@
 ﻿
+using System.Configuration;
+using System;
+using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+
 namespace FinanceTracker
 {
     public partial class FinanceTracker
     {
         private void GeneratePDFReport()
         {
+            try
+            {
+                Document.Create(container =>
+                {
+                    container.Page(page =>
+                    {
+                        page.Size(PageSizes.A4);
+                        page.Header()
+                            .Text("text");
+                    });
+                }).GeneratePdf("../../test.pdf");
 
+                System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["pdfFilePath"]);
+            }
+            catch (Exception ex)
+            {
+                _logger.Error(ex);
+            }
         }
     }
 }
--- FinanceTracker.cs
+++ FinanceTracker.cs
@@ -31,18 +31,7 @@ namespace FinanceTracker
 
         private void buttonGeneratePDFReportClick(object sender, EventArgs e)
         {
-            Document.Create(container =>
-            {
-                container.Page(page =>
-                {
-                    page.Size(PageSizes.A4);
-                    page.Header()
-                        .Text("text");
-                });
-            }).GeneratePdf("../../test.pdf");
-
-            System.Diagnostics.Process.Start(ConfigurationManager.AppSettings["pdfFilePath"]);
-            
+            GeneratePDFReport();
         }
     }
 }
